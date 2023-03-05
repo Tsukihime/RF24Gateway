49073c756aa0eed9eb059c9858b3e63af9903b8d
Tsukihime <vovan7773@gmail.com>
2023-03-05 19:30:45 +0300

    Update radio.cpp

--- src/radio.cpp
+++ src/radio.cpp
@@ -18,7 +18,7 @@ void Radio::init() {
     Serial.println(F("NRF24 radio initialized!"));
  // ------------------------------------------
     radio.enableDynamicPayloads();
-    radio.setDataRate(RF24_250KBPS);   // скорость обмена данными RF24_1MBPS или RF24_2MBPS
+    radio.setDataRate(RF24_1MBPS);   // скорость обмена данными RF24_250KBPS, RF24_1MBPS или RF24_2MBPS
     radio.setCRCLength(RF24_CRC_8);   // размер контрольной суммы 8 bit или 16 bit RF24_CRC_DISABLED RF24_CRC_16
     radio.setChannel(channel);            // установка канала
     radio.setAutoAck(false);            // автоответ
