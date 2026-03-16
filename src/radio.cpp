78b5f43581776da2a03eb8093f09ec29bc4163a8
Tsukihime <vovan7773@gmail.com>
2026-03-16 20:02:27 +0300

    new MQTTGateway library

--- src/radio.cpp
+++ src/radio.cpp
@@ -1,8 +1,14 @@
 #include "radio.h"
 #include <RF24.h>
 #include "MQTT.h"
-#include "RF24MQTTProto.h"
 #include "Config.h"
+#include "lib/MQTTGateway.hpp"
+
+MQTTGatewayReceiver<1024> receiver(
+    [](const char* topic, uint8_t* payload, uint16_t payload_length, bool retained) {
+        MQTT::publish(topic, payload, payload_length, retained);        
+    }
+);
 
 RF24 radio(16, 15);
 const uint8_t address[6] = { "NrfMQ" };
@@ -27,76 +33,21 @@ void Radio::init() {
     radio.startListening();           // приём
 }
 
-bool parsePacket(const Packet& pk, uint8_t payloadSize) {
-    static uint16_t topic_length;
-    static uint16_t payload_length;
-    static bool retained;
-    static uint8_t buffer[1024];
-    static uint16_t pos = 0;
-
-    if (payloadSize == 0) {
-        return false;
-    }
-
-    const uint8_t *data;
-    uint8_t block_sz;
-
-    if (pk.header.marker == Marker::START || pk.header.marker == Marker::START_STOP) {
-        pos = 0;
-        topic_length = pk.first.topic_length;
-        payload_length = pk.first.payload_length;
-        retained = pk.header.retained;
-        data = pk.first.data;
-        block_sz = payloadSize - offsetof(Packet, first.data);
-    } else {
-        data = pk.next.data;
-        block_sz = payloadSize - offsetof(Packet, next.data);
-    }
-
-    if ((pos + block_sz) > sizeof(buffer)) {
-        Serial.println("Buffer overflow!");
-        return false;
-    }
-
-    memcpy(&buffer[pos], data, block_sz);
-    pos += block_sz;
-
-    if (pk.header.marker == Marker::STOP || pk.header.marker == Marker::START_STOP) {
-        uint16_t length = pos;
-        pos = 0;
-
-        if ((payload_length + topic_length) != length) {
-            Serial.println("Parse error!");
-            return false;
-        }
-
-        String topic;
-        topic.reserve(topic_length + 1);
-        for (int i = 0; i < topic_length; i++) {
-            topic += (char)buffer[i];
-        }
-
-        MQTT::publish(topic.c_str(), &buffer[topic_length], payload_length, retained);
-    }
-
-    return true;
-}
-
-void sendUnparsedPacket(const Packet& pk, uint8_t payloadSize) {
+void sendUnparsedPacket(const uint8_t* packet, uint8_t payloadSize) {
     String topic = Config::getRF24GatewayPrefix() + "unparsed";
-    MQTT::publish(topic.c_str(), reinterpret_cast<const uint8_t*>(&pk), payloadSize);
+    MQTT::publish(topic.c_str(), packet, payloadSize);
 }
 
 void Radio::loop() {
-    Packet packet;
+    uint8_t data[32];
     if (!radioOk) return;
 
     while (radio.available()) {
         uint8_t payloadSize = radio.getDynamicPayloadSize();
-        radio.read(&packet, payloadSize);
+        radio.read(data, payloadSize);
 
-        if (!parsePacket(packet, payloadSize)) {
-            sendUnparsedPacket(packet, payloadSize);
+        if (!receiver.parsePacket(data, payloadSize)) {
+            sendUnparsedPacket(data, payloadSize);
         }
     }
 }
